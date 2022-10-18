b8f7f35c949cddbdbc305d9bb5548c3c7141d7d7
FrostElAkuma <68550356+FrostElAkuma@users.noreply.github.com>
2022-10-18 21:08:23 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,101 @@
+//Arabic translation for the Donation page
+function Arabic1(){
+    //We reload the page to go back to english
+    if(document.querySelector(".btnAr").innerHTML == "English")
+    {
+        location.reload()
+    }
+    //Arabic Translation
+    document.querySelector(".btnAr").innerHTML = "English";
+    document.querySelector(".translate1").innerHTML = "خير";
+    document.querySelector(".translate2").innerHTML = "التبرع";
+    document.querySelector(".translate3").innerHTML = "التطوع";
+    document.querySelector(".translate4").innerHTML = "البيئة";
+    document.querySelector(".translate5").innerHTML = "افعل كل الخير في مكان واحد";
+    document.querySelector(".translate6").innerHTML = "اسمك مرادف للكرم";
+    document.querySelector(".translate7").innerHTML = "إطعام المسكين";
+    document.querySelector(".translate8").innerHTML = "ساعد في القضاء على الجوع العالمي بأقل من 1 درهم فقط";
+    for(let i = 0; i< 4; i++){
+        document.getElementsByClassName("translate9")[i].innerHTML = "كمية التبرع";
+        document.getElementsByClassName("translate10")[i].innerHTML = "إرسال";
+      }
+    document.querySelector(".translate11").innerHTML = "سقيا الماء";
+    document.querySelector(".translate12").innerHTML = "أفضل الصدقة سقي الماء";
+    document.querySelector(".translate13").innerHTML = "ساهم في بناء مسجد";
+    document.querySelector(".translate14").innerHTML = "يمكنك المساهمة ب 1 درهم فقط لبناء مسجد  ";
+    document.querySelector(".translate15").innerHTML = "كفالة يتيم";
+    document.querySelector(".translate16").innerHTML = "يمكنك دفع 1درهم فقط لكفالة طفل يتيم ";
+    document.querySelector(".translate17").innerHTML = "اعرض المزيد";
+    document.querySelector(".translate18").innerHTML = "الرجوع إلى البداية";
+    document.querySelector(".translate19").innerHTML = "إتصل بنا";
+    document.querySelector(".translate20").innerHTML = "تابعنا";
+    document.querySelector(".translateX").innerHTML = "المزيد في الطريق";
+}
+
+//Arabic translation for the Volunteer page
+function Arabic2(){
+    //We reload the page to go back to english
+    if(document.querySelector(".btnAr").innerHTML == "English")
+    {
+        location.reload()
+    }
+    //Arabic Translation
+    document.querySelector(".btnAr").innerHTML = "English";
+    document.querySelector(".translate1").innerHTML = "خير";
+    document.querySelector(".translate2").innerHTML = "التبرع";
+    document.querySelector(".translate3").innerHTML = "التطوع";
+    document.querySelector(".translate4").innerHTML = "البيئة";
+    document.querySelector(".translate5").innerHTML = "افعل كل الخير في مكان واحد";
+    document.querySelector(".translate6").innerHTML = "اسمك مرادف للكرم";
+    document.querySelector(".translate7").innerHTML = "إطعام المسكين";
+    document.querySelector(".translate8").innerHTML = "ساعد في القضاء على الجوع العالمي بأقل من 1 درهم فقط";
+    for(let i = 0; i< 4; i++){
+        document.getElementsByClassName("translate9")[i].innerHTML = "رقم الهاتف ";
+        document.getElementsByClassName("translate10")[i].innerHTML = "إرسال";
+      }
+    document.querySelector(".translate11").innerHTML = "سقيا الماء";
+    document.querySelector(".translate12").innerHTML = "أفضل الصدقة سقي الماء";
+    document.querySelector(".translate13").innerHTML = "ساهم في بناء مسجد";
+    document.querySelector(".translate14").innerHTML = "يمكنك المساهمة ب 1 درهم فقط لبناء مسجد  ";
+    document.querySelector(".translate15").innerHTML = "كفالة يتيم";
+    document.querySelector(".translate16").innerHTML = "يمكنك دفع 1درهم فقط لكفالة طفل يتيم ";
+    document.querySelector(".translate17").innerHTML = "اعرض المزيد";
+    document.querySelector(".translate18").innerHTML = "الرجوع إلى البداية";
+    document.querySelector(".translate19").innerHTML = "إتصل بنا";
+    document.querySelector(".translate20").innerHTML = "تابعنا";
+    document.querySelector(".translateX").innerHTML = "المزيد في الطريق";
+}
+
+//Arabic translation for the Envirmonment page
+function Arabic3(){
+    //We reload the page to go back to english
+    if(document.querySelector(".btnAr").innerHTML == "English")
+    {
+        location.reload()
+    }
+    //Arabic Translation
+    document.querySelector(".btnAr").innerHTML = "English";
+    document.querySelector(".translate1").innerHTML = "خير";
+    document.querySelector(".translate2").innerHTML = "التبرع";
+    document.querySelector(".translate3").innerHTML = "التطوع";
+    document.querySelector(".translate4").innerHTML = "البيئة";
+    document.querySelector(".translate5").innerHTML = "افعل كل الخير في مكان واحد";
+    document.querySelector(".translate6").innerHTML = "اسمك مرادف للكرم";
+    document.querySelector(".translate7").innerHTML = "إعادة التدوير";
+    document.querySelector(".translate8").innerHTML = " يسعدنا تزويدك بصناديق إعادة التدوير وسنكون في خدمتك ";
+    for(let i = 0; i< 4; i++){
+        document.getElementsByClassName("translate9")[i].innerHTML = "رقم الهاتف ";
+        document.getElementsByClassName("translate10")[i].innerHTML = "إرسال";
+      }
+    document.querySelector(".translate11").innerHTML = "زراعة الأشجار";
+    document.querySelector(".translate12").innerHTML = " يمكنك المساعدة في زراعة ما لا يزيد عن شجرة واحدة لمساعدة الكوكب";
+    document.querySelector(".translate13").innerHTML = "استبدل الزجاجات البلاستيكية بالزجاجات الزجاجية";
+    document.querySelector(".translate14").innerHTML = "يسعدنا أن نوفر لك قوارير زجاجية لتحل محل الزجاجات البلاستيكية الخاصة بك";
+    document.querySelector(".translate15").innerHTML = "الطاقة النظيفة";
+    document.querySelector(".translate16").innerHTML = "سنكون سعداء لمساعدتك في التحول إلى الطاقة الخضراء ";
+    document.querySelector(".translate17").innerHTML = "اعرض المزيد";
+    document.querySelector(".translate18").innerHTML = "الرجوع إلى البداية";
+    document.querySelector(".translate19").innerHTML = "إتصل بنا";
+    document.querySelector(".translate20").innerHTML = "تابعنا";
+    document.querySelector(".translateX").innerHTML = "المزيد في الطريق";
+}
